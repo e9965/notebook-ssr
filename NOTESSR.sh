07967e30f9d3d3d23fa62730bf923672a1df6b1f
E9965-Kenny <63435466+e9965@users.noreply.github.com>
2020-06-13 19:08:37 +0800

    Update NOTESSR.sh

--- NOTESSR.sh
+++ NOTESSR.sh
@@ -4,33 +4,39 @@ IFS=$(echo -en "\n\b")
 #=========================Variable============================#
 data=$1
 passwd=$2
+red='\033[31m'
+green='\033[32m'
+yellow='\033[33m'
+blue='\033[36m'
+pp='\033[35m'
+endc='\033[0m'
 #=========================Function============================#
 	errhandle(){
 		case $1 in
-		1)echo -e "\033[31m初始化出錯,Ngrok搭建失败,檢查網絡&請回報Bug\033[0m" && exit 2
+		1)echo -e "${red}初始化出錯,Ngrok搭建失败,檢查網絡&請回報Bug${endc}" && exit 2
 			;;
-		2)echo -e "\033[31m無法下載SSR搭建腳本,請檢查網絡並回報Bug\033[0m" && exit 2
+		2)echo -e "${red}無法下載SSR搭建腳本,請檢查網絡並回報Bug${endc}" && exit 2
 			;;
-		3)echo -e "\033[31m未正確啟動Ngrok,請回報Bug\033[0m" && exit 2
+		3)echo -e "${red}未正確啟動Ngrok,請回報Bug${endc}" && exit 2
 			;;
-		4)echo -e "\033[31m未完成搭建SSR,請回報Bug\033[0m" && exit 2
+		4)echo -e "${red}未完成搭建SSR,請回報Bug${endc}" && exit 2
 			;;
-		*)echo -e "\033[31m未知错误,错误代码$1,請回報Bug\033[0m" && exit 128
+		*)echo -e "${red}未知错误,错误代码$1,請回報Bug${endc}" && exit 128
 			;;
 		esac
 	}
 	
 	installtuning(){
 	#Objective: Setup Tuning
-		echo -e "\033[34m========================================\033[0m"
-		echo -e "\033[34m開始初始化...... \033[0m"
+		echo -e "${yellow}========================================${endc}"
+		echo -e "${blue}開始初始化......${endc}"
 		wget -q https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-amd64.zip
 		if [[ $? != 0 ]] ; then errhandle 1 ; fi
 		unzip -q -o ngrok-stable-linux-amd64.zip && rm -f ngrok-stable-linux-amd64.zip
 		./ngrok authtoken $1 > /dev/null 2>&1
 		if [[ $? != 0 ]] ; then errhandle 1 ; fi
-		echo -e "\033[34m完成初始化...... \033[0m"
-		echo -e "\033[34m========================================\033[0m"
+		echo -e "${green}完成初始化......${endc}"
+		echo -e "${yellow}========================================${endc}"
 	}
 	
 	ssr(){
@@ -49,7 +55,7 @@ passwd=$2
 		wget -O tunnels http://127.0.0.1:4040/api/tunnels > /dev/null 2>&1
 		if [[ $? == 0 ]]
 		then
-			echo -e "\033[34m正在獲取SSR鏈接信息: \033[0m"
+			echo -e "${blue}正在獲取SSR鏈接信息:${endc}"
 			raw=$(grep -o "tcp://\{1\}[[:print:]].*,\{1\}" tunnels)
 			raw=${raw##*/}
 			raw=${raw%%\"*}
@@ -58,13 +64,13 @@ passwd=$2
 			pass=$(cat /etc/shadowsocks-r/config.json | grep "password")
 			pass=${pass##*:}
 			pass=${pass%%,*}
-			echo "服務器:\"${adress}\""
-			echo "端口:\"${port}\""
-			echo "密碼:\"${pass}\""
-			echo "混淆:\"http_simple\""
-			echo "方法:\"aes-256-cfb\""
-			echo "協議:\"auth_aes128_md5\""
-			echo -e "\033[34m========================================\033[0m"
+			echo -e "${green}服務器:\"${adress}\"${endc}"
+			echo -e "${green}端口:\"${port}\"${endc}"
+			echo -e "${green}密碼:\"${pass}\"${endc}"
+			echo -e "${green}混淆:\"http_simple\"${endc}"
+			echo -e "${green}方法:\"aes-256-cfb\"${endc}"
+			echo -e "${green}協議:\"auth_aes128_md5\"${endc}"
+			echo -e "${yellow}========================================${endc}"
 		else
 			errhandle 3
 		fi
@@ -75,7 +81,7 @@ passwd=$2
 		seconds_left=$1
 		while [ $seconds_left -gt 0 ]
 		do
-			echo -n -e "\033[34m<<<<距離搭建完成還剩下:${seconds_left}秒>>>>\033[0m"
+			echo -n -e "${blue}<<<<距離搭建完成還剩下:${seconds_left}秒>>>>${endc}"
 			sleep 1
 			seconds_left=$(($seconds_left - 1))
 			echo -ne "\r                                                       \r"
@@ -98,33 +104,34 @@ passwd=$2
 	token=$2
 	if [[ $firstrun == 0 ]] 
 	then
-		echo -e "\033[34m自動安裝中...... \033[0m"
+		echo -e "${green}自動安裝中......${endc}"
 		installtuning $token
-		echo -e "\033[32m開始搭建SSR...... \033[0m"
+		echo -e "${blue}開始搭建SSR......${endc}"
 		ssr $3
 	else
-		echo -e "\033[34m重新開啟SSR中...... \033[0m"
-		/etc/init.d/shadowsocks-r start
+		echo -e "${blue}重新開啟SSR中......${endc}"
+		/etc/init.d/shadowsocks-r restart
+		echo -e "${green}重新開啟SSR成功......${endc}"
 	fi
-	echo -e "\033[34m========================================\033[0m"
-	echo -e "\033[32m開始設置內網穿透...... \033[0m"
+	echo -e "${yellow}========================================${endc}"
+	echo -e "${blue}開始設置內網穿透......${endc}"
 	nohup ./ngrok tcp --region=jp 10086 > /dev/null 2>&1 &
-	echo -e "\033[32m完成設置內網穿透...... \033[0m"
-	echo -e "\033[34m========================================\033[0m"
+	echo -e "${green}完成設置內網穿透......${endc}"
+	echo -e "${yellow}========================================${endc}"
 	if [[ $firstrun == 0 ]] ; then waitcounting 200 ; fi
-	echo -e "\033[33m正在查詢SSR狀態: \033[0m"
+	echo -e "${yellow}正在查詢SSR狀態:${endc}"
 	if [[ ! -f "/etc/init.d/shadowsocks-r" ]]
 	then
 		errhandle 4
 	else
 		/etc/init.d/shadowsocks-r status
 	fi
-	echo -e "\033[34m========================================\033[0m"
+	echo -e "${yellow}========================================${endc}"
 	info
 	}
 #=========================Main_Program============================#
-echo -e "\033[32mNOTESSR 腳本 -ver beta 6.5 \033[0m"
-echo -e "\033[32m========================================\033[0m"
+echo -e "${blue}NOTESSR 腳本 -ver beta 7.0${endc}"
+echo -e "${yellow}========================================${endc}"
 determinate
 if [[ ${data} != "info" ]]
 then
