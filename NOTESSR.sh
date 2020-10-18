c7f0313114e3f4a0e38e2d6488add3a857c68f9f
E9965-Kenny <e9965@nthykyldss.edu.hk>
2020-10-18 20:28:21 +0800

    整合新版本ver1.0.0

--- NOTESSR.sh
+++ NOTESSR.sh
@@ -3,52 +3,129 @@ OLD_IFS=$IFS
 IFS=$(echo -en "\n\b")
 #=========================Variable============================#
 data=$1
+#Token
 passwd=$2
-obfs=$3
+port=10086
+method="chacha20-ietf"
+protocol="auth_sha1_v4"
+obfs="http_simple"
+FILENAME="ShadowsocksR-v3.2.2"
+URL="https://github.com/shadowsocksrr/shadowsocksr/archive/3.2.2.tar.gz"
+BASE=`pwd`
 red='\033[31m'
 green='\033[32m'
 yellow='\033[33m'
 blue='\033[36m'
 pp='\033[35m'
-endc='\033[0m'
+plain='\033[0m'
 #=========================Function============================#
 	errhandle(){
 		case $1 in
-		1)echo -e "${red}初始化出錯,Ngrok搭建失败,檢查網絡&請回報Bug${endc}" && exit 2
+		1) echo -e "${red}初始化出錯,Ngrok搭建失败,檢查網絡&請回報Bug${plain}" && exit 2
 			;;
-		2)echo -e "${red}無法下載SSR搭建腳本,請檢查網絡並回報Bug${endc}" && exit 2
+		2) echo "${red}SSR端口被占用,匯報BUG${plain}" && exit 2
 			;;
-		3)echo -e "${red}未正確啟動Ngrok,請回報Bug${endc}" && exit 2
+		3) echo -e "${red}未正確啟動Ngrok,請回報Bug${plain}" && exit 2
 			;;
-		4)echo -e "${red}未完成搭建SSR,請回報Bug${endc}" && exit 2
+		4) echo -e "${red}未完成搭建SSR,請回報Bug${plain}" && exit 2
 			;;
-		*)echo -e "${red}未知错误,错误代码$1,請回報Bug${endc}" && exit 128
+		*) echo -e "${red}未知错误,错误代码$1,請回報Bug${plain}" && exit 128
 			;;
 		esac
 	}
-	
+
 	installtuning(){
 	#Objective: Setup Tuning
-		echo -e "${yellow}========================================${endc}"
-		echo -e "${blue}開始初始化......${endc}"
+		echo -e "${yellow}========================================${plain}"
+		echo -e "${blue}開始初始化......${plain}"
 		wget -q https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-amd64.zip
 		if [[ $? != 0 ]] ; then errhandle 1 ; fi
 		unzip -q -o ngrok-stable-linux-amd64.zip && rm -f ngrok-stable-linux-amd64.zip
-		./ngrok authtoken $1 > /dev/null 2>&1
+		./ngrok authtoken ${data} > /dev/null 2>&1
 		if [[ $? != 0 ]] ; then errhandle 1 ; fi
-		echo -e "${green}完成初始化......${endc}"
-		echo -e "${yellow}========================================${endc}"
+		echo -e "${green}完成初始化......${plain}"
+		echo -e "${yellow}========================================${plain}"
 	}
 	
+	preinstall(){
+		apt install -y telnet net-tools libsodium18 openssl unzip
+		res=`which wget`
+		[ "$?" != "0" ] && apt install -y wget
+		res=`which netstat`
+		[ "$?" != "0" ] && apt install -y net-tools
+		apt autoremove -y
+		res=`which python`
+		if [ "$?" != "0" ]; then
+			ln -s /usr/bin/python3 /usr/bin/python
+		fi
+	}
+
+	installSSR(){
+    if [ ! -d /usr/local/shadowsocks ]; then
+        if ! wget --no-check-certificate -O ${FILENAME}.tar.gz ${URL}; then
+            errhandle 4
+        fi
+
+        tar -zxf ${FILENAME}.tar.gz
+        mv shadowsocksr-3.2.2/shadowsocks /usr/local
+        if [ ! -f /usr/local/shadowsocks/server.py ]; then
+            cd ${BASE} && rm -rf shadowsocksr-3.2.2 ${FILENAME}.tar.gz
+            errhandle 4
+        fi
+    fi
+
+     cat > /etc/shadowsocksR.json<<-EOF
+{
+    "server":"0.0.0.0",
+    "server_ipv6":"[::]",
+    "server_port":${port},
+    "local_port":1081,
+    "password":"${passwd}",
+    "timeout":600,
+    "method":"${method}",
+    "protocol":"${protocol}",
+    "protocol_param":"",
+    "obfs":"${obfs}",
+    "obfs_param":"",
+    "redirect":"",
+    "dns_ipv6":false,
+    "fast_open":false,
+    "workers":1
+}
+EOF
+
+	cat > /lib/systemd/system/shadowsocksR.service <<-EOF
+[Unit]
+Description=shadowsocksR
+Documentation=NOTESSR
+After=network-online.target
+Wants=network-online.target
+
+[Service]
+Type=forking
+LimitNOFILE=32768
+ExecStart=/usr/local/shadowsocks/server.py -c /etc/shadowsocksR.json -d start
+ExecReload=/bin/kill -s HUP \$MAINPID
+ExecStop=/bin/kill -s TERM \$MAINPID
+
+[Install]
+WantedBy=multi-user.target
+EOF
+
+    systemctl daemon-reload
+    systemctl enable shadowsocksR && systemctl restart shadowsocksR
+    sleep 3
+    res=`netstat -nltp | grep ${port} | grep python`
+    if [ "${res}" = "" ]; then
+        errhandle 2
+    fi
+	}
+
 	ssr(){
 	#Objective: Setup SSR
-		wget -O shadowsocks-all.sh https://raw.githubusercontent.com/e9965/notebook-ssr/master/shadowsocks-all.sh > /dev/null 2>&1
-		if [[ $? == 0 ]]
-		then
-			chmod +x shadowsocks-all.sh && nohup ./shadowsocks-all.sh install $1 > /dev/null 2>&1 &
-		else
-			errhandle 2
-		fi
+		preinstall
+		installSSR
+		cd ${BASE} && rm -rf shadowsocksr-3.2.2 ${FILENAME}.tar.gz
 	}
 	
 	info(){
@@ -56,29 +133,19 @@ endc='\033[0m'
 		wget -O tunnels http://127.0.0.1:4040/api/tunnels > /dev/null 2>&1
 		if [[ $? == 0 ]]
 		then
-			echo -e "${blue}正在獲取SSR鏈接信息:${endc}"
+			echo -e "${blue}正在獲取SSR鏈接信息:${plain}"
 			raw=$(grep -o "tcp://\{1\}[[:print:]].*,\{1\}" tunnels)
 			raw=${raw##*/}
 			raw=${raw%%\"*}
 			adress=${raw%%:*}
-			port=${raw##*:}
-			pass=$(cat /etc/shadowsocks-r/config.json | grep "password")
-			pass=${pass##*:}
-			pass=${pass#*\"}
-			pass=${pass%%,*}
-			pass=${pass%\"*}
-			ssrlinktmp=$(echo -n "${pass}" | base64 -w0 | sed 's/=//g;s/\//_/g;s/+/-/g')
-			fake=$(cat /etc/shadowsocks-r/config-fake.json)
-			fake=$(echo -n "${fake}" | base64 -w0)
-			ssrlink=$(echo -n "${adress}:${port}:auth_sha1_v4:aes-256-cfb:http_simple:${ssrlinktmp}/?obfsparam=${fake}" | base64 -w0)
-			echo -e "${green}服務器:\"${adress}\"${endc}"
-			echo -e "${green}端口:\"${port}\"${endc}"
-			echo -e "${green}密碼:\"${pass}\"${endc}"
-			echo -e "${green}混淆:\"http_simple\"${endc}"
-			echo -e "${green}方法:\"aes-256-cfb\"${endc}"
-			echo -e "${green}協議:\"auth_sha1_v4\"${endc}"
-			echo -e "${green}鏈接:ssr://${ssrlink}${endc}"
-			echo -e "${yellow}========================================${endc}"
+			rport=${raw##*:}
+			echo -e "${green}服務器:\"${adress}\"${plain}"
+			echo -e "${green}端口:\"${rport}\"${plain}"
+			echo -e "${green}密碼:\"${passwd}\"${plain}"
+			echo -e "${green}混淆:\"${obfs}\"${plain}"
+			echo -e "${green}方法:\"${method}\"${plain}"
+			echo -e "${green}協議:\"${protocol}\"${plain}"
+			echo -e "${yellow}========================================${plain}"
 		else
 			errhandle 3
 		fi
@@ -89,7 +156,7 @@ endc='\033[0m'
 		seconds_left=$1
 		while [ $seconds_left -gt 0 ]
 		do
-			echo -n -e "${pp}<<<<距離搭建完成還剩下:${seconds_left}秒>>>>${endc}"
+			echo -n -e "${pp}<<<<距離搭建完成還剩下:${seconds_left}秒>>>>${plain}"
 			sleep 1
 			seconds_left=$(($seconds_left - 1))
 			echo -ne "\r                                                       \r"
@@ -97,55 +164,50 @@ endc='\033[0m'
 	}
 	
 	determinate(){
-	#Objective: Check Whether install or not
-	if [[ ! -f "/etc/init.d/shadowsocks-r" ]]
-	then
-		installed=0
-	else
-		installed=1
-	fi
-	return ${installed}
+		#Objective: Check Whether install or not
+		if [[ ! -f "/etc/shadowsocksR.json" ]]
+		then
+			installed=0
+		else
+			installed=1
+		fi
+		return ${installed}
 	}
 	
 	main(){
-	firstrun=$1
-	token=$2
-	if [[ $firstrun == 0 ]] 
-	then
-		echo -e "${green}自動安裝中......${endc}"
-		installtuning $token
-		echo -e "${blue}開始搭建SSR......${endc}"
-		ssr $3
-	else
-		echo -e "${blue}重新開啟SSR中......${endc}"
-		/etc/init.d/shadowsocks-r restart
-		echo -e "${green}重新開啟SSR成功......${endc}"
-	fi
-	echo -e "${yellow}========================================${endc}"
-	echo -e "${blue}開始設置內網穿透......${endc}"
-	nohup ./ngrok tcp --region=jp 10086 > /dev/null 2>&1 &
-	echo -e "${green}完成設置內網穿透......${endc}"
-	echo -e "${yellow}========================================${endc}"
-	if [[ $firstrun == 0 ]] ; then waitcounting 200 ; fi
-	echo -e "${yellow}正在查詢SSR狀態:${endc}"
-	if [[ ! -f "/etc/init.d/shadowsocks-r" ]]
-	then
-		errhandle 4
-	else
-		/etc/init.d/shadowsocks-r status
-		touch /etc/shadowsocks-r/config-fake.json
-		echo -n "${4}" > /etc/shadowsocks-r/config-fake.json
-	fi
-	echo -e "${yellow}========================================${endc}"
-	info
+		echo -e "${yellow}========================================${plain}"
+		echo -e "${blue}開始設置內網穿透......${plain}"
+		nohup ./ngrok tcp --region=jp 10086 > /dev/null 2>&1 &
+		echo -e "${green}完成設置內網穿透......${plain}"
+		echo -e "${yellow}========================================${plain}"
+		determinate
+		if [[ $? == 0 ]] 
+		then
+			echo -e "${green}自動安裝中......${plain}"
+			installtuning
+			echo -e "${blue}開始搭建SSR......${plain}"
+			ssr > /dev/null 2>&1 & waitcounting 200
+		else
+			echo -e "${blue}重新開啟SSR中......${plain}"
+			systemctl restart shadowsocksR
+			echo -e "${green}重新開啟SSR成功......${plain}"
+		fi
+		echo -e "${yellow}正在查詢SSR狀態:${plain}"
+		if [[ ! -f "/lib/systemd/system/shadowsocksR.service" ]]
+		then
+			errhandle 4
+		else
+			systemctl status shadowsocksR
+		fi
+		echo -e "${yellow}========================================${plain}"
+		info
 	}
 #=========================Main_Program============================#
-echo -e "${blue}NOTESSR -ver 0.5.1 || By:E9965 || 可免流 || ${endc}"
-echo -e "${yellow}========================================${endc}"
-determinate
+echo -e "${blue}|| NOTESSR -ver 1.0.0 || By:E9965 || 可免流 || ${plain}"
+echo -e "${yellow}========================================${plain}"
 if [[ ${data} != "info" ]]
 then
-	main $? $data $passwd $obfs
+	main
 else
 	info
 fi
