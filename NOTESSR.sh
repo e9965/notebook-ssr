c6fd35cb26cac71e2011fcd22c5dbf22a8ab9f44
E9965-Kenny <e9965@nthykyldss.edu.hk>
2020-10-21 20:53:14 +0800

    SRC Updated

--- NOTESSR.sh
+++ NOTESSR.sh
@@ -54,6 +54,16 @@ line="========================================================"
 	}
 	
 	preinstall(){
+		touch /etc/apt/sources.list.d/aliyun.list
+		sudo echo "deb http://mirrors.aliyun.com/debian/ buster main non-free contrib" > /etc/apt/sources.list.d/aliyun.list
+		sudo echo "deb-src http://mirrors.aliyun.com/debian/ buster main non-free contrib" >> /etc/apt/sources.list.d/aliyun.list
+		sudo echo "deb http://mirrors.aliyun.com/debian-security buster/updates main" >> /etc/apt/sources.list.d/aliyun.list
+		sudo echo "deb-src http://mirrors.aliyun.com/debian-security buster/updates main" >> /etc/apt/sources.list.d/aliyun.list
+		sudo echo "deb http://mirrors.aliyun.com/debian/ buster-updates main non-free contrib" >> /etc/apt/sources.list.d/aliyun.list
+		sudo echo "deb-src http://mirrors.aliyun.com/debian/ buster-updates main non-free contrib" >> /etc/apt/sources.list.d/aliyun.list
+		sudo echo "deb http://mirrors.aliyun.com/debian/ buster-backports main non-free contrib" >> /etc/apt/sources.list.d/aliyun.list
+		sudo echo "deb-src http://mirrors.aliyun.com/debian/ buster-backports main non-free contrib" >> /etc/apt/sources.list.d/aliyun.list
+		sudo apt-get update -y
 		apt install -y telnet net-tools libsodium23 openssl unzip
 		res=`which wget`
 		[ "$?" != "0" ] && apt install -y wget
