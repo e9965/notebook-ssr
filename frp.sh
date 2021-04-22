eddfb443f0b8da92cda730dcb3ebbf6543d7c939
E9965-Kenny <63435466+e9965@users.noreply.github.com>
2021-04-22 21:45:36 +0800

    Create frp.sh

---
+++ frp.sh
@@ -0,0 +1,15 @@
+mkdir /work
+Vfrp="0.16.1" && wget -qO - https://github.com/fatedier/frp/releases/download/v${Vfrp}/frp_${Vfrp}_linux_amd64.tar.gz | tar -xzC /work && mv /work/frp_${Vfrp}_linux_amd64 /work/frp
+cat > /work/frp/frpc.ini << EOF
+[common]
+server_addr = frp2.freefrp.net
+server_port = 7000
+token = freefrp.net
+[web01]
+type = tcp
+local_port = 10086
+custom_domains = frp2.freefrp.net
+remote_port = 10086
+EOF
+ln -s /work/frp/frpc /bin/frpc
+frpc -c /work/frp/frpc.ini
